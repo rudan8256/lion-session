9230f9a50878aa13b149d58687702263e0fa0ec9
user <FS>
2021-07-01 17:39:48 +0900

    first_commit

---
+++ bp/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('data published')
+    body = models.TextField()
+
+    writer = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+
+    likes = models.ManyToManyField(User, through='Like',through_fields=('blog','user'),related_name="Likes")
+    
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    body = models.TextField(max_length=500)
+    pub_data = models.DateTimeField('data published')
+    writer= models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+class Like(models.Model): 
+    blog = models.ForeignKey(Blog, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+
